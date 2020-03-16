1fff3f1378c7173037c0dd4c4c99923c20df7219
Ujjwal Shrestha <ushrestha@zipcodes-MBP.fios-router.home>
2020-03-16 17:15:39 -0400

    Need to work on optional task

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,117 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+    Node head = null;
+
+     public class Node{
+        int element;
+        Node nextElement;
+        Node(int d){ element = d;}
+    }
+
+    public void add(int data) {
+      Node current = head;
+        if(current == null){
+            head = new Node(data);
+        }
+        else {
+            while(current.nextElement != null){
+                current = current.nextElement;
+            }
+            current.nextElement = new Node(data);
+        }
+    }
+
+    public void remove(int data) {
+        Node current = head;
+        if (head.element == data) {
+            head = head.nextElement;
+        }
+        while (current.nextElement != null) {
+            if (current.nextElement.element == data) { current.nextElement = current.nextElement.nextElement; }
+            current = current.nextElement;
+        }
+    }
+
+    public boolean contains(int data) {
+        Node current = head;
+        while(current != null) {
+            if (current.element == data) {
+                return true;
+            }current = current.nextElement;
+        }
+        return false;
+    }
+
+    public int find(int data) {
+        Node current = head;
+        int count = 0;
+       while(current != null){
+            if(current.element == data)
+                return count;
+            count++;
+            current = current.nextElement;
+        }
+        return count;
+    }
+
+    public int size() {
+        int count = 0;
+        Node current = head;
+        while(current != null){
+            count++;
+            current = current.nextElement;
+        }
+        return count;
+    }
+
+    public int get(int index) {
+        Node current = head;
+        int count = 0;
+        while(current != null){
+            if(count == index)
+                return current.element;
+            count++;
+            current = current.nextElement;
+        }
+        return -1;
+    }
+
+    public SinglyLinkedList copy() {
+        SinglyLinkedList copyList = new SinglyLinkedList();
+        Node current = head;
+        while(current != null){
+            copyList.add(current.element);
+            current = current.nextElement;
+        }
+        return copyList;
+    }
+
+    public void sort(){
+        Node current = head;
+        Node index = null;
+        int temp;
+
+        if(head == null){ }
+        else{
+            while(current != null){
+                index = current.nextElement;
+                while (index != null){
+                    if(current.element > index.element){
+                        temp = current.element;
+                        current.element = index.element;
+                        index.element = temp;
+                    }
+                    index = index.nextElement;
+                }
+                current = current.nextElement;
+            }
+        }
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,88 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    SinglyLinkedList singly;
+
+    @Before
+    public void init() {
+        singly = new SinglyLinkedList();
+        singly.add(1);
+        singly.add(5);
+        singly.add(2);
+        singly.add(6);
+        singly.add(3);
+        singly.add(4);
+    }
+
+    @Test
+    public void TestAdd() {
+        singly.add(456);
+        Assert.assertTrue(singly.contains(456));
+    }
+
+    @Test
+    public void TestRemove() {
+        singly.remove(2);
+        Assert.assertFalse(singly.contains(2));
+    }
+
+    @Test
+    public void TestContains(){
+        Assert.assertFalse(singly.contains(10));
+        Assert.assertTrue(singly.contains(5));
+    }
+
+    @Test
+    public void TestFind(){
+        Assert.assertEquals(4, singly.find(3));
+        Assert.assertEquals(1, singly.find(5));
+        Assert.assertEquals(0, singly.find(1));
+    }
+
+    @Test
+    public void TestSize(){
+        Assert.assertEquals(6,singly.size());
+    }
+    @Test
+    public void TestSize2(){
+        singly.add(7);
+        singly.add(10);
+        singly.add(22);
+        singly.add(8);
+        Assert.assertEquals(10,singly.size());
+    }
+
+    @Test
+    public void TestGet(){
+        Assert.assertEquals(2,singly.get(2));
+        Assert.assertEquals(3,singly.get(4));
+    }
+
+    @Test
+    public void TestCopy(){
+        SinglyLinkedList copyList = new SinglyLinkedList();
+        copyList.add(1);
+        copyList.add(6);
+        copyList.add(2);
+        copyList.add(5);
+        copyList.add(3);
+        copyList.add(4);
+        Assert.assertEquals(5, copyList.copy().get(3));
+    }
+
+    @Test
+    public void TestSort(){
+        singly.sort();
+        Assert.assertEquals(3, singly.get(2));
+        Assert.assertEquals(5, singly.get(4));
+        Assert.assertEquals(6, singly.get(5));
+    }
 }
